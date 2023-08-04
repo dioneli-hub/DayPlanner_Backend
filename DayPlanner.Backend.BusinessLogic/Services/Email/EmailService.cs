cea0ace700b0b1f61b7d24d83f2a3f489dcb1292
Diana Levchenko <dianka_levchenko@inbox.ru>
2023-08-04 21:24:41 +0200

    Initial ResetPassword email template built.

--- DayPlanner.Backend.BusinessLogic/Services/Email/EmailService.cs
+++ DayPlanner.Backend.BusinessLogic/Services/Email/EmailService.cs
@@ -11,13 +11,14 @@ namespace DayPlanner.Backend.BusinessLogic.Services
     public class EmailService : IEmailService
     {
         //add sender email info as well as api address of the endpoint to verify to appsettings json
-       
+
         private readonly DataContext _context;
 
-        public EmailService(DataContext context) {
+        public EmailService(DataContext context)
+        {
             _context = context;
         }
-        
+
 
         public async Task SendVerificationEmail(int userId)
         {
@@ -30,15 +31,8 @@ namespace DayPlanner.Backend.BusinessLogic.Services
                 email.Subject = "DayPlanner Email Verification";
                 var bodyBuilder = new BodyBuilder();
                 bodyBuilder.HtmlBody = GetHTMLVerificationTemplate(user);
-
                 email.Body = bodyBuilder.ToMessageBody();
-            //    email.Body = new TextPart(MimeKit.Text.TextFormat.Html)
-            //    {
-            //        Text = $"<h1>Hey, {user.FirstName} {user.LastName}! Here is your verification token. Copy it and paste at the website in order to activate your account.</h1>"+
-            //        "<h2> Please, do not share this token with anyone.</h2>" +
-            //        $"{user.VerificationToken}",
-            //};
-
+     
                 using var smtp = new SmtpClient();
                 smtp.Connect("smtp.outlook.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
                 await smtp.AuthenticateAsync("dianka_levchenko@outlook.com", "Lbfyf1mamapapacats");//this.FromEmail, this.FromEmailPswd
@@ -58,13 +52,17 @@ namespace DayPlanner.Backend.BusinessLogic.Services
                 email.To.Add(MailboxAddress.Parse(user.Email));// user.Email //"round.world@bk.ru"
                 email.Subject = "DayPlanner Reset Password";
 
-                email.Body = new TextPart(MimeKit.Text.TextFormat.Html)
-                {
-                    Text = $"<h1>Hey, {user.FirstName} {user.LastName}! Here is your reset password token. Copy it and paste at the website in order to set a new password.</h1>" +
-                    "<h2> Please, do not share this token with anyone.</h2>" +
-                    $"{user.ResetPasswordToken}",
+                var bodyBuilder = new BodyBuilder();
+                bodyBuilder.HtmlBody = GetHTMLResetPasswordTemplate(user);
 
-                };
+                email.Body = bodyBuilder.ToMessageBody();
+                //email.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+                //{
+                //    Text = $"<h1>Hey, {user.FirstName} {user.LastName}! Here is your reset password token. Copy it and paste at the website in order to set a new password.</h1>" +
+                //    "<h2> Please, do not share this token with anyone.</h2>" +
+                //    $"{user.ResetPasswordToken}",
+
+                //};
 
                 using var smtp = new SmtpClient();
                 smtp.Connect("smtp.outlook.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
@@ -77,7 +75,7 @@ namespace DayPlanner.Backend.BusinessLogic.Services
 
         private string GetHTMLVerificationTemplate(User user)
         {
-             return $@"
+            return $@"
             <!DOCTYPE html>
             <html>
             <head>
@@ -86,14 +84,14 @@ namespace DayPlanner.Backend.BusinessLogic.Services
                 <title>DayPlanner: Verify Your Email</title>
                 <meta name='viewport' content='width=device-width, initial-scale=1'>
                 <style>
-                    
+                   
                 </style>
             </head>
             <body>
                 <div class='container'>
                     <h1>Email Verification</h1>
-                    <p>Daer {user.FirstName} {user.LastName},</p>
-                    <p>Thank you for joining us in DayPlanner. In order to accomplish your registration process, click the verification button below:</p>
+                    <p>Dearr {user.FirstName} {user.LastName},</p>
+                    <p>Thank you for joining us at DayPlanner. In order to accomplish your registration process, click the verification button below:</p>
                     <p>
                         <a href='http://localhost:4200/verify?token={Uri.EscapeDataString(user.VerificationToken)}' class='button' type='button'>Verify</a>
                     </p>
@@ -103,5 +101,50 @@ namespace DayPlanner.Backend.BusinessLogic.Services
             </html>";
         }
 
+        private string GetHTMLResetPasswordTemplate(User user)
+        {
+            return $@"
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <meta charset='utf-8'>
+                <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+                <title>DayPlanner: Reset Password</title>
+                <meta name='viewport' content='width=device-width, initial-scale=1'>
+                <style>
+                    
+                </style>
+            </head>
+            <body>
+                <div class='container'>
+                    <h1>Reset Password</h1>
+                    <p>Dear {user.FirstName} {user.LastName},</p>
+                    <p>You are trying to reset password at DayPlanner. In order to accomplish the process, please, click the Reset Password button below:</p>
+                    <p>
+                        <a href='http://localhost:4200/reset-password?token={Uri.EscapeDataString(user.ResetPasswordToken)}' class='button' type='button'>Reset Password</a>
+                    </p>
+                    <p>Or just copy the token and paste it at the website:</p>
+                    <p><b>{user.ResetPasswordToken}</b></p>
+                    <p><i>Please, ignore this email if you did not try to reset your password at DayPlanner.</i></p>
+                </div>
+            </body>
+            </html>";
+        }
+
+        private string GetCssStyles()
+        {
+            return $@"
+                      
+                body, p, h1, h2, h3, h4, h5, h6 {{ margin: 0; padding: 0;}}
+                body {{font-family: Arial, sans-serif; line-height: 1.6; background-color: #f5f5f5;}}
+                .container {{ max-width: 600px; margin: 0 auto; padding: 20px; background-color: #ffffff;border: 1px solid #e0e0e0;border-radius: 5px; box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);}}
+                .header {{text-align: center;padding-bottom: 20px;}} 
+                .logo {{max - width: 100px;}}
+                .content {{padding: 20px;background-color: #f9f9f9;border-radius: 5px;}}
+                .button {{display: inline-block;background-color: #007bff;color: #ffffff; padding: 10px 20px;text-decoration: none;border-radius: 5px;}}
+                .footer {{text-align: center;margin-top: 20px;color: #999999;}}
+
+            ";
+        }
     }
 }
